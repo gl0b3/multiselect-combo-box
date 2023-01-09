abbfccc6329c1352559e679e6dac11b5ba823bb6
Kótay-Szabó Károly <kkotay@intrend.hu>
2023-01-09 22:28:13 +0100

    Type+Enter didn't select, focusOut didn't clear

--- src/multiselect-combo-box.js
+++ src/multiselect-combo-box.js
@@ -132,14 +132,17 @@ import './multiselect-combo-box-input.js';
     constructor() {
       super();
       this._boundCustomOverlaySelectedItemChanged = this._customOverlaySelectedItemChanged.bind(this);
+      this._boundFocusOut = this._focusOut.bind(this);
     }
 
     ready() {
       super.ready();
 
       // replace listener to modify default behavior
+      // console.log(this.$.input);
       this.$.comboBox.$.overlay.removeEventListener('selection-changed', this.$.comboBox._boundOverlaySelectedItemChanged);
       this.$.comboBox.$.overlay.addEventListener('selection-changed', this._boundCustomOverlaySelectedItemChanged);
+      this.$.input.addEventListener('focusout', this._boundFocusOut);
 
       // modify check to allow custom renderers
       this.$.comboBox.$.overlay._isItemSelected = this._customIsSelected.bind(this);
@@ -325,6 +328,7 @@ import './multiselect-combo-box-input.js';
     }
 
     _comboBoxValueChanged(event, selectedItem) {
+      console.log('')
       const item = selectedItem || this.$.comboBox.selectedItem;
       if (!item) {
         return;
@@ -340,17 +344,23 @@ import './multiselect-combo-box-input.js';
 
       if (!selectedItem) {
         this.$.comboBox.value = null;
-      } else {
-        this.selectedItems = update;
-      }
+      } 
+
+      this.selectedItems = update;
 
       if (this.validate()) {
         this._dispatchChangeEvent();
       }
 
+      this.$.input.value = null;
+      
       // reset the focus index, so a value-change event
       // is not fired when the overlay is closed
       this.$.comboBox._focusedIndex = -1;
+    }
+
+    _focusOut(event) {
+      this.$.comboBox.value = null;
     }
 
     _handleCustomValueSet(event) {
@@ -379,19 +389,23 @@ import './multiselect-combo-box-input.js';
     }
 
     _findIndex(item, selectedItems, itemIdPath) {
+      // console.log('item: ' + item + ', selectedItems: ' + selectedItems + ', itemIdPath: ' + itemIdPath);
       if (itemIdPath && item) {
         for (let index = 0; index < selectedItems.length; index++) {
           if (selectedItems[index] && selectedItems[index][itemIdPath] === item[itemIdPath]) {
             return index;
           }
         }
+        // console.log('return -1');
         return -1;
       } else {
+        // console.log('selectedItems.indexOf(' + item + '): ' + selectedItems.indexOf(item));
         return selectedItems.indexOf(item);
       }
     }
 
     _handleItemRemoved(event) {
+      console.log(Object.assign(event, CustomEvent).type);
       const item = event.detail.item;
       const update = this.selectedItems.slice(0);
       update.splice(update.indexOf(item), 1);
@@ -450,6 +464,7 @@ import './multiselect-combo-box-input.js';
     }
 
     _customOverlaySelectedItemChanged(event) {
+      console.log('_customOverlaySelectedItemChanged');
       event.stopPropagation();
 
       if (event.detail.item instanceof ComboBoxPlaceholder) {
